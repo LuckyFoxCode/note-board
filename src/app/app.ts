d3b83546cf61c4127b24a8daf324b5fbbc68273f
LuckyFoxCode <sergei181989@gmail.com>
2026-01-08 17:18:10 +0200

    refactor: complete step 13 derived state selectors

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,4 @@
 import { bindOpenCategoryModal } from '@/events';
-import { addNote, getFilteredNotes, state } from '@/store';
 import { renderLayout } from '@/ui';
 
 export function initApp() {
@@ -10,17 +9,4 @@ export function initApp() {
 
   bindOpenCategoryModal(addCategoryBtn);
   root.append(sidebar, board);
-
-  const obj = {
-    title: '111',
-    excerpt: 'asd',
-    tags: [{ name: '1', color: '1' }],
-    categoryId: '1',
-  };
-
-  const data = addNote(state.app, obj);
-  const filter = getFilteredNotes(state.app, 'categoryId', '2');
-
-  console.log('--- data ---', data);
-  console.log('--- filter ---', filter);
 }
--- src/store/selectors.ts
+++ src/store/selectors.ts
@@ -1,9 +1,18 @@
 import type { AppState, Note } from '@/types';
+import type { FilterableNoteField, NotesStats } from './state.type';
 
-export function getFilteredNotes(
+export function getFilteredNotes<K extends FilterableNoteField>(
   state: AppState,
-  field: keyof Note,
-  value: Note[keyof Note],
+  field: K,
+  value: Note[K],
 ): Note[] {
   return state.notes.filter((note) => note[field] === value);
+}
+
+export function getStatsNotes(state: AppState): NotesStats {
+  return {
+    totalNotes: state.notes.length,
+    totalActiveNotes: state.notes.filter((note) => !note.archived).length,
+    totalArchivedNotes: state.notes.filter((note) => note.archived).length,
+  };
 }
