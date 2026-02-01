a697fa83e11565a511fa10d5e5ba2711ef997bb3
LuckyFoxCode <sergei181989@gmail.com>
2026-02-01 17:37:20 +0200

    refactor: remove unused files

--- src/persistence/StorageProvider.ts
+++
@@ -1,7 +0,0 @@
-import type { PersistedState } from './PersistedState';
-
-export interface StorageProvider {
-  load(): PersistedState | null;
-  save(state: PersistedState): void;
-  clear(): void;
-}
--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -1,3 +1,1 @@
 export * from './LocalStorageProvider';
-export * from './PersistedState';
-export * from './StorageProvider';
